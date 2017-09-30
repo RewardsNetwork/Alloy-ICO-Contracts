25740adae4c3897201eb94939cf6a4c1ab606025
Shoaib <shoaib@codemojo.io>
2017-09-30 16:17:27 +0530

    Added support for multiple minting agents who can Mint

--- contracts/token/MintableToken.sol
+++ contracts/token/MintableToken.sol
@@ -16,12 +16,23 @@ import '../ownership/Ownable.sol';
 contract MintableToken is StandardToken, Ownable {
   event Mint(address indexed to, uint256 amount);
   event MintFinished();
+  event MintingAgentChanged(address addr, bool state  );
 
   bool public mintingFinished = false;
 
+  /** List of agents that are allowed to create new tokens */
+  mapping (address => bool) public mintAgents;
 
   modifier canMint() {
     require(!mintingFinished);
+    _;
+  }
+
+  modifier onlyMintAgent() {
+    // Only whitelisted contracts are allowed to mint new tokens
+    if(!mintAgents[msg.sender]) {
+      revert();
+    }
     _;
   }
 
@@ -31,12 +42,20 @@ contract MintableToken is StandardToken, Ownable {
    * @param _amount The amount of tokens to mint.
    * @return A boolean that indicates if the operation was successful.
    */
-  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+  function mint(address _to, uint256 _amount) onlyMintAgent canMint public returns (bool) {
     totalSupply = totalSupply.add(_amount);
     balances[_to] = balances[_to].add(_amount);
     Mint(_to, _amount);
     Transfer(0x0, _to, _amount);
     return true;
+  }
+
+  /**
+   * Owner can allow a contract to mint new tokens.
+   */
+  function setMintAgent(address addr, bool state) onlyOwner canMint public {
+    mintAgents[addr] = state;
+    MintingAgentChanged(addr, state);
   }
 
   /**

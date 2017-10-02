a4c253652d4833770b3ff34c6f696b523766aee2
Shoaib <shoaib@codemojo.io>
2017-10-02 12:19:03 +0530

    Added a hard cap on total tokens that can be minted

--- contracts/AlloyToken.sol
+++ contracts/AlloyToken.sol
@@ -14,8 +14,19 @@ contract AlloyToken is MintableToken, VestedToken {
     string constant public symbol = 'ALLOY';
     uint constant public decimals = 18;
 
+    // Limits the supply to 100mn ALLOYs
+    uint constant public maximumSupply = 100000000 ether;
+
     function AlloyToken(){
 
     }
 
+    /**
+     * Overrides the default minting function
+     * Hard cap of 100 mn ALLOYs in total that can be minted
+     */
+    function mint(address _to, uint256 _amount) onlyMintAgent canMint public returns (bool) {
+        require(totalSupply.add(_amount) <= maximumSupply);
+        return super.mint(_to, _amount);
+    }
 }
